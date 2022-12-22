20fb543a0e2c01cc5fd4b69d77bc46640fe9193c
David Verghese <davidverghese@davidvehesesmbp.myfiosgateway.com>
2022-12-22 11:52:05 -0500

    correct the name of class from Shop to Audio_Engineer

--- models.py
+++ models.py
@@ -4,7 +4,7 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
-class Shop(Base):
+class Audio_Engineer(Base):
    __tablename__ = 'audio_engineers'
 
    id = Column(Integer(), primary_key=True)
