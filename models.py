4b6afd9c30b4e20fe7a98de33ec12704b9127728
David Verghese <davidverghese@davidvehesesmbp.myfiosgateway.com>
2022-12-22 11:35:27 -0500

    create audio_engineers model

---
+++ models.py
@@ -0,0 +1,23 @@
+from sqlalchemy import create_engine, func
+from sqlalchemy import Column, DateTime, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class Shop(Base):
+   __tablename__ = 'audio_engineers'
+
+   id = Column(Integer(), primary_key=True)
+   name = Column(String())
+   created_at = Column(DateTime(), server_default=func.now())
+   updated_at = Column(DateTime(), onupdate=func.now())
+
+   def __repr__(self):
+       return f'Shop(id={self.id}, name="{self.name}")'
+
+if   __name__ == '__main__':
+		engine = create_engine('sqlite:///audio_engineers.db')
+		Base.metadata.create_all(engine)
+
+
+
